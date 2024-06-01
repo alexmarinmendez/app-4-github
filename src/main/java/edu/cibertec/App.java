3b3bf4fa7739ee515988dbe11838ae3bf91c44b4
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-05-31 22:06:15 -0500

    actualizacion del año en el mensaje

--- src/main/java/edu/cibertec/App.java
+++ src/main/java/edu/cibertec/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola Cibertec" );
+        System.out.println( "Hola Cibertec 2024" );
     }
 }
